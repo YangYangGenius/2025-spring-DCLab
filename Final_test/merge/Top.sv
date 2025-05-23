a142bffea8eeaff458284aaff18ac28c3b2b1b39
Yang.io <b10901023yang@gmail.com>
2025-05-23 14:40:40 +0800

    準備測試merge

--- Final_test/merge/Top.sv
+++ Final_test/merge/Top.sv
@@ -48,94 +48,96 @@ parameter STOP_ADDR_COUNT = 20'h1FFFF; //四秒
 
 
 //-------------------------------addr control-------------------------------
-logic [19:0] addr_r, addr_w;
+    logic [19:0] addr_r, addr_w;
 
-always_comb begin
-    addr_w = addr_r;
-    if (addr_r == STOP_ADDR_COUNT)          addr_w = 20'h0;
-    else if (state_r == S_LEFT && i_AUD_DACLRCK)  addr_w = addr_r + 1'b1;   
-end
+    always_comb begin
+        addr_w = addr_r;
+        if (addr_r == STOP_ADDR_COUNT)                  addr_w = 20'h0;
+        else if (state_r == S_LEFT && i_AUD_DACLRCK)    addr_w = addr_r + 1'b1;   
+    end
 
 
 
 //-------------------------------stop control-------------------------------
-logic stop_r, stop_w;
-always_comb begin
-    if (addr_r == STOP_ADDR_COUNT) stop_w = 1'b1;
-    else stop_w = 1'b0;
-end
+    logic stop_r, stop_w;
+    always_comb begin
+        if (addr_r == STOP_ADDR_COUNT) stop_w = 1'b1;
+        else stop_w = 1'b0;
+    end
+
 //-------------------------------fetch control-------------------------------
-parameter MUSIC_1_BEGIN = 20'h20000;
-parameter MUSIC_1_END   = 20'h3FFFF; //四秒
-parameter MUSIC_2_BEGIN = 20'h40000;
-parameter MUSIC_2_END   = 20'h5FFFF; //四秒
-parameter MUSIC_3_BEGIN = 20'h60000;
-parameter MUSIC_3_END   = 20'h7FFFF; //四秒
-parameter MUSIC_4_BEGIN = 20'h80000;
-parameter MUSIC_4_END   = 20'h9FFFF; //四秒
-parameter NUM_MUSIC = 4;
+    parameter MUSIC_1_BEGIN = 20'h20000;
+    parameter MUSIC_1_END   = 20'h3FFFF; //四秒
+    parameter MUSIC_2_BEGIN = 20'h40000;
+    parameter MUSIC_2_END   = 20'h5FFFF; //四秒
+    parameter MUSIC_3_BEGIN = 20'h60000;
+    parameter MUSIC_3_END   = 20'h7FFFF; //四秒
+    parameter MUSIC_4_BEGIN = 20'h80000;
+    parameter MUSIC_4_END   = 20'h9FFFF; //四秒
+    parameter NUM_MUSIC = 4;
 
-wire [19:0] addr_fetch;
-logic [NUM_MUSIC-1:0] music_count_r, music_count_w;
+    wire [19:0] addr_fetch;
+    logic [NUM_MUSIC-1:0] music_count_r, music_count_w;
 
-assign addr_fetch = {music_count_r, 16'h0000} + addr_r; //取出音樂資料的地址
+    assign addr_fetch = {music_count_r, 16'h0000} + addr_r; //取出音樂資料的地址
 
-always_comb begin
-    if (state = S_LEFT) music_count_w = 4'h2;
-    else if (state_r == S_RIGHT && music_count_r <= 4'h8) music_count_w = music_count_r + 4'h2;
-    else music_count_w = music_count_r;
-end
+    always_comb begin
+        if (state = S_LEFT) music_count_w = 4'h2;
+        else if (state_r == S_RIGHT && music_count_r <= 4'h8) music_count_w = music_count_r + 4'h2;
+        else music_count_w = music_count_r;
+    end
 
 //-------------------------------SRAM control-------------------------------
 
-assign o_SRAM_ADDR = addr_fetch;
-assign io_SRAM_DQ  = 16'dz; // sram_dq as output
+    assign o_SRAM_ADDR = addr_fetch;
+    assign io_SRAM_DQ  = 16'dz; // sram_dq as output
 
-assign o_SRAM_WE_N = 1'b1; //SRAM_WE_N 設定目前操作模式， 0 為寫， 1 為讀 
-assign o_SRAM_CE_N = 1'b0;
-assign o_SRAM_OE_N = 1'b0;
-assign o_SRAM_LB_N = 1'b0;
-assign o_SRAM_UB_N = 1'b0;
+    assign o_SRAM_WE_N = 1'b1; //SRAM_WE_N 設定目前操作模式， 0 為寫， 1 為讀 
+    assign o_SRAM_CE_N = 1'b0;
+    assign o_SRAM_OE_N = 1'b0;
+    assign o_SRAM_LB_N = 1'b0;
+    assign o_SRAM_UB_N = 1'b0;
 
+//-------------------------------Output & Merge control-------------------------------
 
+    //在DE2-115上， SRAM的讀取是同一個cycle，但還是建議往後延遲一個cycle
+    logic [15:0] read_data_r, read_data_w;
+    wire  [15:0] merge_data;
+    wire  [1:0]  merge_mode;
 
+    assign merge_mode = 2'b0;
 
-
-
-//-------------------------------Merge control-------------------------------
-
-//TODO:SRAM是在丟出地址同時得到答案還是要等一個cycle呢?
-logic [15:0] read_data_r, read_data_w;
-always_comb begin
-    if (state_r == S_RIGHT) begin
-        if () read_data_w = io_SRAM_DQ; 
-        else 
+    always_comb begin
+        if (state_r == S_RIGHT && music_count_r <= 4'h8) begin
+            read_data_w = io_SRAM_DQ;  
+        end
+        else read_data_w = 16'b0; 
     end
-    else read_data_w = read_data_r; 
-end
 
-//-------------------------------Output control-------------------------------
-logic [15:0] data_play;
-assign data_play = 
-
+    Merge merge0(
+        .i_clk(i_clk),
+        .i_rst_n(i_rst_n),
+        .i_mode(merge_mode),
+        .i_data(read_data_r),
+        .o_data(merge_data)
+    );
 
 //-------------------------------Output monitor-------------------------------
 
-assign o_time = {2'b00, addr_r[19:15]};
+    assign o_time = {2'b00, addr_r[19:15]};
 
-wire[9:0] mili_0;
-wire [14:0] a_extended;
-wire [14:0] sum;
-assign mili_0 = addr_r[14:5];
-assign a_extended = {5'b0, mili_0}; // Extend a to 15 bits by adding 5 leading zeros
-assign sum = a_extended + (a_extended << 3) + (a_extended << 4); // Multiply by 25
-assign o_time_2 = sum[14:8];
+    wire[9:0] mili_0;
+    wire [14:0] a_extended;
+    wire [14:0] sum;
+    assign mili_0 = addr_r[14:5];
+    assign a_extended = {5'b0, mili_0}; // Extend a to 15 bits by adding 5 leading zeros
+    assign sum = a_extended + (a_extended << 3) + (a_extended << 4); // Multiply by 25
+    assign o_time_2 = sum[14:8];
 
-assign o_ledg = (state_r == S_IDLE) ? 4'b1111 :
-				(state_r == S_I2C)  ? 4'b0001 : 
-				(state_r == S_WAIT) ? 4'b0010 : 
-				(state_r == S_LEFT) ? 4'b0100 : 4'b1000;
-
+    assign o_ledg = (state_r == S_IDLE) ? 4'b1111 :
+                    (state_r == S_I2C)  ? 4'b0001 : 
+                    (state_r == S_WAIT) ? 4'b0010 : 
+                    (state_r == S_LEFT) ? 4'b0100 : 4'b1000;
 
 //-------------------------------I2C control-------------------------------
 
@@ -166,7 +168,7 @@ assign o_ledg = (state_r == S_IDLE) ? 4'b1111 :
         .i_rst_n(i_rst_n),
         .i_bclk(i_AUD_BCLK),
         .i_daclrck(i_AUD_DACLRCK),			
-        .i_dac_data(data_play), 	//dac_data
+        .i_dac_data(merge_data), 	//dac_data
         .o_aud_dacdat(o_AUD_DACDAT)
     );
 
@@ -205,6 +207,11 @@ assign o_ledg = (state_r == S_IDLE) ? 4'b1111 :
         else begin
             state_r <= state_w;
             i2c_start_r <= i2c_start_w;
+            read_data_r <= read_data_w;
+            music_count_r <= music_count_w;
+            addr_r <= addr_w;
+            stop_r <= stop_w;
+            data_play_r <= data_play_w;
         end
     end
 
---
+++ Final_test/merge/merge.sv
@@ -0,0 +1,30 @@
+module Merge(
+    input i_clk,
+    input i_rst_n,
+    input i_mode,
+    input  [15:0] i_data,
+    output [15:0] o_data
+);
+
+logic [15:0] data_r, data_w;
+
+assign o_data = data_r;
+
+always_comb begin
+    data_w = data_r;
+    if (i_data != 16'b0) begin
+        data_w = i_data + data_r;
+    end
+end
+
+always_ff @(posedge i_clk or negedge i_rst_n) begin
+    if (!i_rst_n) begin
+        data_r <= 16'b0;
+    end else begin
+        data_r <= data_w;
+    end
+end
+
+
+
+endmodule
